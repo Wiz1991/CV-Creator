fa5a2c0c89974266ab54dce3aa83e6275d46761e
Wiz1991 <wizoftime92@gmail.com>
2020-10-10 18:36:01 +0200

    Updates

---
+++ precache-manifest.44a9bea16a9d76433a04dab08a59b4b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8779952fc2e3803388f6894abeb775c4",
+    "url": "/CV-Creator/index.html"
+  },
+  {
+    "revision": "10af56c87dc1440863ea",
+    "url": "/CV-Creator/static/css/main.7519176b.chunk.css"
+  },
+  {
+    "revision": "7e83ffb461434098d777",
+    "url": "/CV-Creator/static/js/2.93b88410.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CV-Creator/static/js/2.93b88410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10af56c87dc1440863ea",
+    "url": "/CV-Creator/static/js/main.c7d18b1a.chunk.js"
+  },
+  {
+    "revision": "191d91a4386e2154ad69",
+    "url": "/CV-Creator/static/js/runtime-main.738b30e5.js"
+  }
+]);
